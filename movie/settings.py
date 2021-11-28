f651d3112e5ced62097d16c427cd5d248039375a
Alkesh Kothar <77409891+AlkeshKothar@users.noreply.github.com>
2021-11-28 16:01:46 +0530

    deploy

--- movie/settings.py
+++ movie/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 from datetime import timedelta
 
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h4yv(v)^l&np6p&(l1)7-t-b4&$22%k6*h11csi%+ui%bbp602'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -162,3 +162,6 @@ SWAGGER_SETTINGS = {
 
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+
+django_heroku.settings(locals())
